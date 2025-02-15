b538f293e364db4129e39a47417960b1dff3a6a3
JoaoGSantiago <gustavo2jg@gmail.com>
2025-02-15 02:34:03 -0300

    script certo

--- script.sh
+++ script.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+set -e
 
-
-#update no sistema e instalação zabbix
+#atualização e instalação
 sudo apt update -y && sudo apt upgrade -y
-wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu20.04_all.deb
-dpkg -i zabbix-release_latest_7.2+ubuntu20.04_all.deb
-apt update
 
-#instalação mysql e proxy do zabbix
-apt install -y mysql-server zabbix-proxy-mysql zabbix-sql-scripts zabbix-agent
+sudo apt install -y mysql-server
 
-#configuração do mysql
+sudo systemctl enable mysql
+sudo systemctl start mysql
+
+curl -O https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu20.04_all.deb
+sudo apt install -y ./zabbix-release_6.0-4+ubuntu20.04_all.deb
+sudo apt update -y
+sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
+
+#configuração database e zabbix
 sudo mysql -e "CREATE DATABASE zabbix CHARACTER SET utf8 COLLATE utf8_bin;"
-sudo mysql -e "CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'zabbix_pass';"
+sudo mysql -e "CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'teste123';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';"
-sudo mysql -e "set global log_bin_trust_function_creators = 1;"
 sudo mysql -e "FLUSH PRIVILEGES;"
 
-#importção do esquema inicial
-cat /usr/share/zabbix/sql-scripts/mysql/proxy.sql | sudo mysql -u zabbix -pzabbix_pass zabbix
+zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | sudo mysql -u zabbix -pteste123 zabbix
 
-#configuração do zabbix para o mysql
-sudo sed -i "s/# DBPassword=/DBPassword=zabbix_pass/" /etc/zabbix/zabbix_proxy.conf
+sudo sed -i 's/# DBPassword=/DBPassword=teste123/' /etc/zabbix/zabbix_server.conf
 
-sudo systemctl restart mysql
-sudo systemctl restart zabbix-proxy
-sudo systemctl enable zabbix-proxy
-sudo systemctl enable mysql zabbix-agent
+#iniciando serviço
+sudo systemctl restart zabbix-server zabbix-agent apache2
+sudo systemctl enable zabbix-server zabbix-agent apache2
+
